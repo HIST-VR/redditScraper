7f629da47f7bca0706c9a89e25c6d4aca72afb2d
HIST-VR <xx.hist.xx@gmail.com>
2022-09-29 15:13:20 +0300

    code clean up

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os.path
 from datetime import datetime
 
 import praw
-import telebot
+
 from googletrans import Translator
 
 import tokens
+
 #  set time
 current_datetime = datetime.now()
 set_day = 86400
@@ -18,10 +19,9 @@ reddit = tokens.reddit
 
 #  telegram token
 tg_bot = tokens.tg_bot
-# bruh
+
 
 #                     ----------------------TRANSLATING----------------------
-# print(translator.translate('안녕하세요', dest='ru'))
 translator = Translator()
 
 
@@ -32,7 +32,7 @@ def translate_title(title, language):
 #                     ------------------------SCRAPING------------------------
 # get posts information
 def scrape_reddit(sub, amount):
-    #  if time before previous scape less than an hour -> don't scrape again
+    # if time before previous scape less than an hour -> don't scrape again
     if os.path.isfile('hot_posts.json') == False or (
             int(os.path.getmtime("hot_posts.json") + set_hour) < int(current_datetime.timestamp())):
         posts = []
@@ -52,12 +52,13 @@ def scrape_reddit(sub, amount):
         return 'hot_posts.json'
 
 
-# scrape_reddit('HistoryPorn', 3)
+# scrape_reddit('History', 3)
 
 #                      --------------------------BOTING--------------------------
 @tg_bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "HistoryPorn":
+        print('Someone used it!', datetime.now())
         scrape_reddit('HistoryPorn', 10)
         f = open('hot_posts.json', 'r', encoding='utf8')
         data = json.loads(f.read())
@@ -70,42 +71,10 @@ def get_text_messages(message):
 
     elif message.text == "/help":
         tg_bot.send_message(message.from_user.id, "Напиши HistoryPorn")
+
     else:
         tg_bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 
 tg_bot.polling(none_stop=True, interval=0)
 
-# q = 'bitcoin' #query
-# sub = 'CryptoCurrency' #subreddit -- Multiple subreddits can be combined with a +.
-# sort = "top"                        #e.g. sub='CryptoCurrency+SatoshiStreetBets'
-# limit = 50
-#
-# top_posts = r.subreddit(sub).search(q, sort=sort, limit=limit) #creating a search tot the subreddit
-#
-# total_posts = list()
-#
-# #Iterate over the top_posts, and for each instance, create a dictionary to store
-# #the scraped data in an ordered collection. Also, for each instance, append the
-# #newly created dictionary into the empty list.
-#
-# for post in top_posts:
-#     print(vars(post)) # print all properties
-#     Title = post.title,
-#     Score = post.score,
-#     Number_Of_Comments = post.num_comments,
-#     Publish_Date = post.created,
-#     Link = post.permalink,
-#     data_set = {"Title":Title[0],"Score":Score[0],   "Number_Of_Comments":Number_Of_Comments[0],"Publish_Date":Publish_Da te[0],"Link":'https://www.reddit.com'+Link[0]}
-# total_posts.append(data_set)
-#
-# # create csv file
-# df = pd.DataFrame(total_posts)
-# df.to_csv('data.csv', sep=',', index=False)
-#
-# #create json file
-# json_string = json.dumps(total_posts)
-# jsonFile = open("data.json", "w")
-# jsonFile.write(json_string)
-# jsonFile.close()
-#
